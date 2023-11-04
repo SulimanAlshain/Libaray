3d2b07d22d0b77b81839838a8692fed2be73b35a
Awaab Elamin <AwaabElamin@gmail.com>
2023-11-04 14:08:58 -0500

    validate employee form data

--- PresentationLayer/Admin/EmployeeForm.xaml.cs
+++ PresentationLayer/Admin/EmployeeForm.xaml.cs
@@ -23,5 +23,51 @@ namespace PresentationLayer.Admin
         {
             InitializeComponent();
         }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            bool isFormDataValid = validateForm();
+            if (!isFormDataValid) {
+                lblFormMessage.Content = "لسه الشغل دا ما فاليد";
+                return;
+            }
+            lblFormMessage.Content = "كدا نقدر نفول فاليد :)";
+        }
+
+        private bool validateForm()
+        {
+            if (txtGivenName.Text.Length == 0) {
+                lblErrorGivenName.Content = "Given Name Require";
+                return false; 
+            }
+            if (txtFamilyName.Text.Length == 0) {
+                lblErrorFamilyName.Content = "Family Name Require";
+                return false; 
+            }
+            if (txtPhoneName.Text.Length == 0) {
+                lblErrorPhone.Content = "Phone Require";
+                return false; 
+            }
+            if (txtEmail.Text.Length == 0) {
+                lblErrorEmail.Content = "Email Require";
+                return false; 
+            }
+            if (txtPassword.Text.Length == 0) {
+                lblErrorPassword.Content = "Password Require";
+                return false; 
+            }
+            if (txtRole.Text.Length == 0) {
+                lblErrorRole.Content = "Role Require";
+                return false; 
+            }
+            lblErrorGivenName.Content = "";
+            lblErrorFamilyName.Content = "";
+            lblErrorPhone.Content = "";
+            lblErrorEmail.Content = "";
+            lblErrorPassword.Content = "";
+            lblErrorRole.Content = "";
+            return true;
+
+        }
     }
 }
